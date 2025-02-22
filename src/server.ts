28d49fadaeeb2d721c67ab0eebba06bcf3ec6876
Dmytro Andriushchenko <spaser15@outlook.com>
2025-02-22 23:21:43 +0200

    fix(mongodb): run mongodb connection properly, rearrange imports, comment out authRouter

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,15 @@ import express, { type Express } from "express";
 import helmet from "helmet";
 import { pino } from "pino";
 
-import { openAPIRouter } from "@/api-docs/openAPIRouter";
-import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter";
-import { userRouter } from "@/api/user/userRouter";
+import { openAPIRouter } from "@/api-docs/openAPIRouter.js";
+// import { authRouter } from "@/api/auth/authRouter.js";
+import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter.js";
+import { userRouter } from "@/api/user/userRouter.js";
 // import { taskRouter } from "@/api/task/taskRouter";
-import errorHandler from "@/common/middleware/errorHandler";
-import rateLimiter from "@/common/middleware/rateLimiter";
-import requestLogger from "@/common/middleware/requestLogger";
-import { env } from "@/common/utils/envConfig";
-import { connectMongoDB } from "@/common/utils/mongoUtils";
+import errorHandler from "@/common/middleware/errorHandler.js";
+import rateLimiter from "@/common/middleware/rateLimiter.js";
+import requestLogger from "@/common/middleware/requestLogger.js";
+import { env } from "@/common/utils/envConfig.js";
 
 const logger = pino({ name: "server start" });
 const app: Express = express();
@@ -19,7 +19,10 @@ const app: Express = express();
 // Set the application to trust the reverse proxy
 app.set("trust proxy", true);
 
-connectMongoDB();
+(async () => {
+  const { connectMongoDB } = await import("@/common/utils/mongoUtils.js");
+  await connectMongoDB();
+})();
 
 // Middlewares
 app.use(express.json());
@@ -33,6 +36,7 @@ app.use(requestLogger);
 
 // Routes
 app.use("/health-check", healthCheckRouter);
+// app.use("/auth", authRouter);
 app.use("/users", userRouter);
 //app.use('/tasks', taskRouter);
 
