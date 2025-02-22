7bf8846ea5eece9d42fd74cb200af9b3e2dd1c80
Dmytro Andriushchenko <spaser15@outlook.com>
2025-02-22 23:23:21 +0200

    fix(mongodb): add shutdown for mongodb, reformat code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { env } from "@/common/utils/envConfig";
-import { app, logger } from "@/server";
+import { env } from "@/common/utils/envConfig.js";
+import { shutDownMongoDB } from "@/common/utils/mongoUtils.js";
+import { app, logger } from "@/server.js";
 
 const server = app.listen(env.PORT, () => {
   const { NODE_ENV, HOST, PORT } = env;
@@ -8,10 +9,14 @@ const server = app.listen(env.PORT, () => {
 
 const onCloseSignal = () => {
   logger.info("sigint received, shutting down");
+
+  shutDownMongoDB();
+
   server.close(() => {
     logger.info("server closed");
     process.exit();
   });
+
   setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
 };
 
